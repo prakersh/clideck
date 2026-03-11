4240a9640c54ff3587c8f4104494ab568556ccb7
Prakersh Maheshwari <prakersh@live.com>
2026-03-11 19:06:59 +0530

    suppress node sqlite experimental warning on startup

--- db/index.js
+++ db/index.js
@@ -1,13 +1,39 @@
-const { DatabaseSync } = require('node:sqlite');
 const { DB_PATH } = require('../paths');
 const { runMigrations } = require('./migrate');
 
 let db = null;
+let DatabaseSync = null;
+
+function loadDatabaseSync() {
+  if (DatabaseSync) return DatabaseSync;
+
+  const originalEmitWarning = process.emitWarning;
+  process.emitWarning = function emitWarningPatched(warning, ...args) {
+    const warningName = typeof warning === 'object' && warning?.name
+      ? warning.name
+      : typeof args[0] === 'string'
+        ? args[0]
+        : '';
+    const message = typeof warning === 'string' ? warning : warning?.message || '';
+
+    if (warningName === 'ExperimentalWarning' && /sqlite/i.test(message)) return;
+    return originalEmitWarning.call(process, warning, ...args);
+  };
+
+  try {
+    ({ DatabaseSync } = require('node:sqlite'));
+  } finally {
+    process.emitWarning = originalEmitWarning;
+  }
+
+  return DatabaseSync;
+}
 
 function openDb() {
   if (db) return db;
 
-  db = new DatabaseSync(DB_PATH);
+  const SqliteDatabaseSync = loadDatabaseSync();
+  db = new SqliteDatabaseSync(DB_PATH);
   db.exec('PRAGMA journal_mode = WAL;');
   db.exec('PRAGMA synchronous = NORMAL;');
   db.exec('PRAGMA foreign_keys = ON;');
